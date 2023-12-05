5f6c6bbba5b7cd0182298d0c21e443c840581136
Minh Nguyen <minh.nguyen@ocit.ocgov.com>
2023-12-05 13:11:43 -0800

    Added switch between component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
+import { useState } from "react";
 import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
 import SideBar from "./components/SideBar";
 
 function App() {
+
+  const [projectsState, setProjectsState] = useState({
+    selectedProject: undefined, // doing nothing
+    projects: []
+  });  
+
+  const handleStartAddProject = () => {
+
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProject: null, // adding project
+      }
+    })
+  }
+
+  let content;
+
+  if(projectsState.selectedProject === null) {
+    content = <NewProject />
+  } else if(projectsState.selectedProject === undefined) {
+    content = <NoProjectSelected onStartAdProject={handleStartAddProject} />
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <SideBar />
-      <NewProject />
+      <SideBar onStartAdProject={handleStartAddProject} />
+      {content}      
     </main>
   );
 }
